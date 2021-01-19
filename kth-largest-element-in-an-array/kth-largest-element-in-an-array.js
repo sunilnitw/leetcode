0af0eb6dcc1f1485f6b9242c567def205e95fc69
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2021-01-19 22:56:59 +0530

    Time: 88 ms (57.50%), Space: 39.1 MB (86.59%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,6 +4,34 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    nums.sort((a, b)=> Number(a)-Number(b));
-    return nums[nums.length-k];
-};
+    function swap(arr, i, j){
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    function maxify(arr, i, length){
+        let l = i*2+1, r = i*2+2, min = i;
+        if(l<length && arr[min]<arr[l]){
+            min = l;
+        }
+        if(r<length && arr[min]<arr[r]){
+            min = r;
+        }
+        if(min != i){
+            swap(arr, min, i);
+            maxify(arr, min, length);
+        }
+    }
+    function kthLargest(arr, k){
+        let kth = arr[0], length = arr.length;
+        for(let i=Math.floor(length/2)-1;i>=0; i--){
+            maxify(arr, i, length);
+        }
+        for(let i=0; i<k; i++){
+            swap(arr, 0, length-i-1);
+            maxify(arr, 0, length-i-1);
+        }
+        return arr[length-k];
+    }
+    return kthLargest(nums, k);
+    // nums.sort((a, b)=> Number(a)-Number(b));
