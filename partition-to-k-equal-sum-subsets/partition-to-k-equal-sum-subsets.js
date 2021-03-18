dbc13dfc6ed4268cd161cdc69c27a4cf5d001d7a
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2021-03-18 23:22:28 +0530

    Time: 92 ms (50.63%), Space: 38 MB (99.58%) - LeetHub

---
+++ partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var canPartitionKSubsets = function(nums, k) {
+    const sum = nums.reduce((a, b)=>a+b, 0);
+    if(sum%k === 1 || nums.length < k) return false;
+    nums.sort((a,b)=>a-b);
+    let len = nums.length, target = sum/k, right = len-1;
+    if(nums[len-1]>target)return false;
+    while(right>=0 && nums[right] === target){
+        k--;
+        right--;
+    }
+    const partition = new Array(k).fill(0), isPartition = false;
+    function isEqual(){
+        if(isPartition)return true;
+        const a = nums[0];
+        for(let num of partition){
+            if(num !== a) return false;
+        }
+        isPartition = true;
+        return true;
+    }
+    function doPartition(partition, right, nums, target){
+        if(right<0)return true;
+        let tempV = nums[right--];
+        for(let i=0; i<k; i++){
+            if(partition[i]+tempV <= target){
+                partition[i]+=tempV;
+                if(doPartition(partition, right, nums, target)) return true;
+                partition[i]-=tempV;
+            }
+            if(partition[i] === 0)break;
+        }
+        return false;
+    }
+    return doPartition(partition, right, nums, target);
+};
+/*
+1,2,2,3,3,4,5
+*/
