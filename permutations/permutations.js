28d36f7ff30c9ea9893a1536d5c5521c8a5006f7
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2021-03-21 15:33:58 +0530

    Time: 100 ms (51.69%), Space: 41.9 MB (49.31%) - LeetHub

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -1,28 +1,22 @@
-var perm = [];
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var permute = function(nums) {
-    perm = [];
-    generatePermutation(nums, [], 0);
-    return perm;
+    let arr = [];
+    function generate(nums, i){
+       if(i===nums.length){
+            arr.push([...nums]);
+       }
+        for(let j=i; j<nums.length; j++){
+            swap(nums, j, i);
+            generate(nums, i+1);
+            swap(nums, j, i);
+        }
+    }
+    function swap(nums, i, j){
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    generate(nums, 0);
+    return arr;
 };
-​
-var generatePermutation = function(nums, arr, pos){
-    if(pos == nums.length){
-        perm.push(JSON.parse(JSON.stringify(arr)));
-    }
-    for(let i=pos; i<nums.length; i++){
-        arr[pos] = nums[i];
-        swap(nums, i, pos);
-        generatePermutation(nums, arr, pos+1)
-        swap(nums, i, pos);
-    }
-}
-​
-var swap = function(nums, i, j){
-    let temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-}
