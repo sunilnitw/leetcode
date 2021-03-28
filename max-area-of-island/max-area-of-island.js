2df72a24e65bc92d9a4bdd1d0ceb355c67a89d63
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2021-03-28 12:28:29 +0530

    Time: 96 ms (87.69%), Space: 40.6 MB (92.84%) - LeetHub

---
+++ max-area-of-island/max-area-of-island.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    function countMax(i, j, r, c){
+        if(i<0 || j<0 || i>=r || j>=c || grid[i][j] === 0 || grid[i][j] === 2)return 0;
+        
+        grid[i][j] = 2;
+        return 1+countMax(i+1, j, r, c)+countMax(i, j+1, r, c)+countMax(i-1, j, r, c)+countMax(i, j-1, r, c);
+    }
+    
+    let r = grid.length, c = grid[0].length, maxCount = 0;
+    for(let i=0; i<r; i++){
+        for(let j=0; j<c; j++){
+            if(grid[i][j] === 1){
+                maxCount = Math.max(maxCount, countMax(i, j, r, c));
+            }
+        }
+    }
+    return maxCount;
+};
