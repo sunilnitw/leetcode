2a477cc9c79e477a9b5c93408af3e655a34820c6
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2021-04-07 23:17:08 +0530

    Time: 80 ms (77.21%), Space: 39.6 MB (50.45%) - LeetHub

--- jump-game/jump-game.js
+++ jump-game/jump-game.js
@@ -3,33 +3,17 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    let dp = [];
-    function isReached(start, length){
-        if(dp[start] === false)return false;
-        if(start>=length)return true;
-        if(start<length && (start+nums[start] >= length-1 || dp[start] === true)) return true;
-        for(let i=start+nums[start]; i<length  && i>start; i--){
-            dp[start] = isReached(i,length);
-            if(dp[start] === true)return true;
+    let len = nums.length, pos = len-1;
+    
+    if(len <= 1)return true;
+    if(nums[0] === 0) return false;
+    
+    for(let i=len-2; i>=0; i--){
+        if(nums[i]+i >= pos){
+            pos = i;
         }
-        return false;
+        // console.log(i, pos);
     }
-    function canWeJump(nums){
-        let lastPos = nums.length-1;
-        for(let i=lastPos; i>=0; i--){
-            if(i+nums[i]>=lastPos){
-                lastPos = i;
-            }
-        }
-        return lastPos === 0;
-    }
-    return canWeJump(nums);
-    return isReached(0, nums.length);
+    // console.log(pos)
+    return pos === 0 ? true : false;
 };
-
-/*
-[100]
-[2,0]
-[2,3,1,1,4]
-[3,2,1,0,4]
-*/
