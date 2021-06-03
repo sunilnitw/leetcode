b94cbe50bad9a2f71965845e04ec967648c31fa2
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2021-06-03 06:52:16 +0530

    Time: 72 ms (98.85%), Space: 38.4 MB (98.28%) - LeetHub

--- interleaving-string/interleaving-string.js
+++ interleaving-string/interleaving-string.js
@@ -5,30 +5,24 @@
  * @return {boolean}
  */
 var isInterleave = function(s1, s2, s3) {
-    if(s1.length + s2.length !== s3.length)return false;
-    let hash = {};
-    function isValidInterleave(s1, s2, s3, l1, l2, l3, p1, p2, p3){
-        
-        if(p3 === l3)return (p1 === l1 && p2 === l2);
-        
-        let key = p1+"$"+p2+"$"+p3;
-        if(hash.hasOwnProperty(key))return hash[key];
-        if(p1 === l1){
-            return hash[key] = s2[p2] === s3[p3] ? isValidInterleave(s1, s2, s3, l1, l2, l3, p1, p2+1, p3+1) : false;
+    if((s1.length + s2.length) !== s3.length)return false;
+    let cache = {};
+    function compare(i, j, k){
+        let key = `${i}_${j}`;
+        if(s3.length === k)return true;
+        if(cache.hasOwnProperty(key)) return cache[key];
+        if(s1.charAt(i) === s3.charAt(k) && s2.charAt(j) === s3.charAt(k)){
+            // console.log('both equal',i,j,k);
+            return (cache[key] = compare(i+1, j, k+1) || compare(i, j+1, k+1));
+        } else if(s1.charAt(i) === s3.charAt(k)){
+            // console.log('left equal',i,j,k);
+            return (cache[key] = compare(i+1, j, k+1));
+        } else if(s2.charAt(j) === s3.charAt(k)){
+            // console.log('right equal',i,j,k);
+            return (cache[key] = compare(i, j+1, k+1));
+        } else {
+            return false;
         }
-        
-        if(p2 === l2){
-            return hash[key] = s1[p1] === s3[p3] ? isValidInterleave(s1, s2, s3, l1, l2, l3, p1+1, p2, p3+1) : false;
-        }
-        
-        let first = false, second = false;
-        if(s1[p1] === s3[p3]){
-            first = isValidInterleave(s1, s2, s3, l1, l2, l3, p1+1, p2, p3+1);
-        }
-        if(s2[p2] === s3[p3]){
-            second = isValidInterleave(s1, s2, s3, l1, l2, l3, p1, p2+1, p3+1);
-        }
-        return hash[key] = first || second;
     }
-    return isValidInterleave(s1.split(''), s2.split(''), s3.split(''), s1.length, s2.length, s3.length, 0,0,0);
+    return compare(0, 0, 0);
 };
