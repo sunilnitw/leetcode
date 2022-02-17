b8c23978d91d0290535760c9d4428465e4ed5fdc
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-02-17 20:51:22 +0530

    Time: 84 ms, Memory: 44.9 MB - LeetHub

--- find-pivot-index/find-pivot-index.js
+++ find-pivot-index/find-pivot-index.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    if(nums.length == 0) return -1;
     const sum = nums.reduce((a,b)=>a+b);
     let leftSum = 0, rightSum = sum, i = 0;
     while(i<nums.length){
