0d344aef15acbf9f52cd04e2ee86ec4ee3be91ce
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-02-21 21:37:22 +0530

    Time: 132 ms (94.43%), Space: 55.3 MB (48.66%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -3,6 +3,38 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
+    // return quickSort(nums);
+    return mergeSort(nums);
+};
+
+const mergeSort = (nums)=>{
+    const merge = (l, m, r)=>{
+        let arr = new Array(r-l+1), i = l, j = m+1, k =0;
+        while(i<=m && j<=r){
+            if(nums[i]>=nums[j]) arr[k]=nums[j++];
+            else arr[k]=nums[i++];
+            k++;
+        }
+        while(i<=m)arr[k++]=nums[i++];
+        while(j<=r)arr[k++]=nums[j++];
+
+        for(let i=0, j = l; i<(r-l+1);i++, j++){
+            nums[j] = arr[i];
+        }
+    }
+    const divide = (l, r)=>{
+        if(l<r){
+            let m = l+Math.floor((r-l)/2);
+            divide(l,m);
+            divide(m+1, r);
+            merge(l,m,r);
+        }
+    }
+    divide(0, nums.length-1);
+    return nums;
+}
+
+const quickSort = (nums)=>{
     const partition = (l, r)=>{
         let i=l, j = r;
         while(i<j){
@@ -24,4 +56,4 @@ var sortArray = function(nums) {
     }
     quick(0, nums.length-1);
     return nums;
-};
+}
