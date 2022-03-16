ca277ed8129a6981bc443c8adcf9fdefbc1f23a0
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-03-16 22:55:30 +0530

    Time: 68 ms (88.99%), Space: 45.1 MB (5.52%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -11,12 +11,20 @@
  */
 var reverseList = function(head) {
     if(!head || !head.next) return head;
-    let newHead = null;
-    while(head){
-        let temp = head.next;
-        head.next = newHead;
-        newHead = head;
-        head = temp;
-    }
-    return newHead;
+    let nextEle = head.next;
+    head.next = null;
+    let temp = reverseList(nextEle);
+    nextEle.next = head;
+    return temp;
+    
+    
+    // if(!head || !head.next) return head;
+    // let newHead = null;
+    // while(head){
+    //     let temp = head.next;
+    //     head.next = newHead;
+    //     newHead = head;
+    //     head = temp;
+    // }
+    // return newHead;
 };
