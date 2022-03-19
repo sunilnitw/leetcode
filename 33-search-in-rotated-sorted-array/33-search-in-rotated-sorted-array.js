40516408cd3cde67e5d61166924ddcc5ebf40d94
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-03-19 20:08:46 +0530

    Time: 90 ms (44.79%), Space: 42.4 MB (23.88%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -8,18 +8,18 @@ var search = function(nums, target) {
     while(l<=r){
         m = parseInt((l+r)/2);
         if(nums[m] === target) return m;
-        if(nums[l] === target) return l;
-        if(nums[r] === target) return r;
+        // if(nums[l] === target) return l;
+        // if(nums[r] === target) return r;
         // console.log(l,m,r)
-        if(nums[l]<nums[m]){
-            if(target>nums[l] && target<nums[m]){
-                r = m-1;
+        if(nums[l]<=nums[m]){
+            if(target>=nums[l] && target<=nums[m]){
+                r = m;
             } else {
                 l = m+1;
             }
         } else {
-            if(target>nums[m] && target<nums[r]){
-                l = m+1;
+            if(target>=nums[m] && target<=nums[r]){
+                l = m;
             } else {
                 r = m-1;
             }
