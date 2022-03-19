d3a5ff86ccd443d6b492019dc8614765950164d1
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-03-19 19:43:25 +0530

    Time: 64 ms (84.81%), Space: 42.3 MB (45.36%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
@@ -10,17 +10,9 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if(!head || !head.next) return head;
-    let newHead = head.next, prev = null;
-    while(head && head.next){
-        let temp = head.next, next = head.next.next;
-        if(prev){
-            prev.next = temp;
-        }
-        head.next = next;
-        temp.next = head;
-        prev = head;
-        head = next;
-    }
-    return newHead;
+    if(!head || !head.next)return head;
+    let v1=head, v2 = head.next, v3 = v2.next;
+    v2.next = v1;
+    v1.next = swapPairs(v3);
+    return v2;
 };
