ee2cab63d8edf4fdd6b02391cba9c8c7b1758b63
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-03-22 00:22:08 +0530

    Time: 178 ms (45.81%), Space: 47.7 MB (78.21%) - LeetHub

--- 77-combinations/77-combinations.js
+++ 77-combinations/77-combinations.js
@@ -6,12 +6,10 @@
 var combine = function(n, k) {
     let ans = [];
     const comb = (pos, l, arr)=>{
-        // console.log('hhh: ', pos, l, arr)
         if(pos === k){
             ans.push([...arr]);
             return;
         }
-        // console.log(pos,l, arr)
         for(let i=l; i<=n; i++){
             arr[pos] = i;
             comb(pos+1, i+1, arr);
