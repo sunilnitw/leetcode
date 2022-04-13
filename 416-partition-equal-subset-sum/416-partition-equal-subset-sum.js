4b6d7f77a1c9314639453ea6afc288e80e5ffb0a
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-04-13 21:41:06 +0530

    Time: 91 ms (96.62%), Space: 43.8 MB (92.73%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -3,32 +3,6 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
-   return bottomup(nums);
-};
-
-const bottomup = (nums)=>{
-    const sum = nums.reduce((acc, val)=>acc+val,0);
-    if(sum%2 === 1) return false;
-    if(sum === 0) return true;
-    
-    const dp = [new Array(sum/2+1).fill(false), new Array(sum/2+1).fill(false)];
-    dp[0][0] = true;
-    dp[1][0] = true;
-    for(let i=0; i<nums.length; i++){
-        for(let j=1; j<=sum/2; j++){
-            if(nums[i]<=j){
-                dp[(i+1)%2][j] = dp[i%2][j] || dp[i%2][j-nums[i]];
-            } else {
-                dp[(i+1)%2][j] = dp[i%2][j];
-            }
-        }
-        // console.log(dp)
-    }
-    
-    return dp[nums.length%2][sum/2];
-}
-
-const topdown = (nums)=>{
     const sum = nums.reduce((acc, val)=>acc+val,0);
     if(sum%2 === 1) return false;
     if(sum === 0) return true;
@@ -41,4 +15,4 @@ const topdown = (nums)=>{
         return (cache[target] = (find(pos+1, target-nums[pos]) || find(pos+1, target)));
     }
     return find(0, sum/2);
-}
+};
