19104981ac5ecf9f975c086c09601af5e8a22103
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-04-14 22:41:29 +0530

    Time: 73 ms (100.00%), Space: 43.1 MB (33.33%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.js
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.js
@@ -4,28 +4,32 @@
  * @return {boolean}
  */
 var canPartitionKSubsets = function(nums, k) {
-    const sum = nums.reduce((acc, val)=>acc+val,0);
-    if(sum%k !== 0) return false;
+    const sum = nums.reduce((a, b)=>a+b, 0);
+    if(sum%k === 1 || nums.length < k) return false;
+    nums.sort((a,b)=>a-b);
+    let len = nums.length, target = sum/k, right = len-1;
+    if(nums[len-1]>target)return false;
+    while(right>=0 && nums[right] === target){
+        k--;
+        right--;
+    }
+    const partition = new Array(k).fill(0), isPartition = false;
     
-    let visited = new Array(nums.length).fill(false);
-    
-    const canPartition = (pos, curr_sum, target, k)=>{
-        
-        if(k === 1) return true;
-        if(curr_sum === target){
-            return canPartition(0, 0, target, k-1);
-        }
-        if(curr_sum>target) return false;
-        for(let i=pos; i<nums.length; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                if(canPartition(i+1, curr_sum+nums[i], target, k))
-                    return true;
-                visited[i] = false;
+    function doPartition(partition, right, nums, target){
+        if(right<0)return true;
+        let tempV = nums[right--];
+        for(let i=0; i<k; i++){
+            if(partition[i]+tempV <= target){
+                partition[i]+=tempV;
+                if(doPartition(partition, right, nums, target)) return true;
+                partition[i]-=tempV;
             }
+            if(partition[i] === 0)break;
         }
         return false;
-    };
-    
-    return canPartition(0, 0, sum/k, k)
+    }
+    return doPartition(partition, right, nums, target);
 };
+/*
+1,2,2,3,3,4,5
+*/
