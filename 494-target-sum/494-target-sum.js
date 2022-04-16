730159e1aff2cdcd9d17a2e5c87d37aa9e3edda6
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-04-16 14:47:50 +0530

    Time: 287 ms (71.91%), Space: 52.9 MB (18.89%) - LeetHub

--- 494-target-sum/494-target-sum.js
+++ 494-target-sum/494-target-sum.js
@@ -4,10 +4,13 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
+    const cache = {};
     const findSum = (pos, sum)=>{
+        const key = `${pos}_${sum}`;
         if(sum === target && pos === nums.length) return 1;
         if(pos>=nums.length) return 0;
-        return findSum(pos+1, sum+nums[pos])+findSum(pos+1, sum-nums[pos]);
+        if(cache.hasOwnProperty(key)) return cache[key];
+        return (cache[key] = findSum(pos+1, sum+nums[pos])+findSum(pos+1, sum-nums[pos]));
     }
     return findSum(0, 0);
 };
