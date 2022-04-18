dd2e47fcc839825c49d50233301964ea077e9211
Sunil Kumar <37491829+sunilnitw@users.noreply.github.com>
2022-04-18 22:37:00 +0530

    Time: 73 ms (99.27%), Space: 42.5 MB (63.34%) - LeetHub

--- 494-target-sum/494-target-sum.js
+++ 494-target-sum/494-target-sum.js
@@ -4,13 +4,32 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
-    const cache = {};
-    const findSum = (pos, sum)=>{
-        const key = `${pos}_${sum}`;
-        if(sum === target && pos === nums.length) return 1;
-        if(pos>=nums.length) return 0;
-        if(cache.hasOwnProperty(key)) return cache[key];
-        return (cache[key] = findSum(pos+1, sum+nums[pos])+findSum(pos+1, sum-nums[pos]));
+    const sum = nums.reduce((acc, val)=>acc+val,0);
+    if(Math.abs(target)>sum || (target+sum)%2 !== 0) return 0;
+    const newTarget = (target+sum)/2;
+    let dp = [new Array(newTarget+1).fill(0), new Array(newTarget+1).fill(0)];
+    dp[0][0] = 1;
+    dp[1][0] = 1;
+    
+    for(let i=1; i<=nums.length; i++){
+        for(let j=0; j<=newTarget; j++){
+            if(nums[i-1]<=j){
+                dp[i%2][j] = dp[(i-1)%2][j] + dp[(i-1)%2][j-nums[i-1]];
+            } else {
+                dp[i%2][j] =  dp[(i-1)%2][j]; 
+            }
+        }
     }
-    return findSum(0, 0);
+    
+    return dp[nums.length%2][newTarget];
+    
+    // const cache = {};
+    // const findSum = (pos, sum)=>{
+    //     const key = `${pos}_${sum}`;
+    //     if(sum === target && pos === nums.length) return 1;
+    //     if(pos>=nums.length) return 0;
+    //     if(cache.hasOwnProperty(key)) return cache[key];
+    //     return (cache[key] = findSum(pos+1, sum+nums[pos])+findSum(pos+1, sum-nums[pos]));
+    // }
+    // return findSum(0, 0);
 };
